6bf1b4a46d96b5c496b61495b00cbca804e58d7c
Lambert Francis <wetaker1@gmail.com>
2021-07-02 18:34:44 -1000

    Mentions now included in tweet bodies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+
+    def addHandlesToBody(body)
+        # Takes a tweet body and returns the same body but with mentions replaced by links to the mentioned user 
+        words = body.split(' ')
+        words = words.map do |word|
+            if word[0] == '@'
+                potentialUser = word[1..-1]
+                potentialUser = User.find_by(username: potentialUser)
+                if potentialUser
+                    "<a class=\"mention\" href=\"/users/#{potentialUser.id}\">@#{potentialUser.username}</a>"
+                else
+                    word
+                end
+            else
+                word
+            end
+        end
+        words.join(' ')
+    
+    end
+
+
 end
